19d6946dbea39bb356bbe91e612116958edc0063
Denis <63392847+DENisProd@users.noreply.github.com>
2024-12-03 00:28:22 +0300

    try fix airflow 2

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
-    docker compose -f ./airflow/docker-compose.yml build
-    docker compose -f ./airflow/docker-compose.yml up -d
+docker compose -f ./backend/docker-compose.yml up -d
 
-    docker compose -f ./superset/docker-compose.yml build
-    docker compose -f ./superset/docker-compose.yml up -d
+docker compose -f ./superset/docker-compose.yml build
+docker compose -f ./superset/docker-compose.yml up -d
 
-    docker compose -f ./backend/docker-compose.yml up -d
+docker compose -f ./airflow/docker-compose.yml build
+docker compose -f ./airflow/docker-compose.yml up -d
+
