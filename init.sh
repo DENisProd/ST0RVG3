7405038f17386c485578bac5c13d6863d8d6e513
Denis <63392847+DENisProd@users.noreply.github.com>
2024-12-03 15:22:28 +0300

    try fix airflow 7

--- init.sh
+++ init.sh
@@ -6,8 +6,10 @@ docker compose -f ./superset/docker-compose.yml up -d
 docker compose -f ./airflow/docker-compose.yml build
 docker compose -f ./airflow/docker-compose.yml up -d
 
-docker network create airflow_net
+docker network connect airflow_default postgres_store
 
-docker network connect airflow_net airflow-airflow-webserver-1
-docker network connect airflow_net postgres_store
+# docker network create airflow_net
+
+# docker network connect airflow_net airflow-airflow-webserver-1
+# docker network connect airflow_net postgres_store
 
